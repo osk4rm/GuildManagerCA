d8e90dbd3d6a4f470265817d0de7e95ec6765b0f
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-09-12 11:31:52 +0200

    added abstract classes for entity and aggregate root ids

---
+++ GuildManagerCA.Domain/Common/Models/AggregateRootId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuildManagerCA.Domain.Common.Models
+{
+    public abstract class AggregateRootId<TId> : EntityId<TId>
+    {
+        protected AggregateRootId(TId value) : base(value)
+        {
+        }
+    }
+}
---
+++ GuildManagerCA.Domain/Common/Models/EntityId.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuildManagerCA.Domain.Common.Models
+{
+    public abstract class EntityId<TId> : ValueObject
+    {
+        public TId Value { get; }
+
+        protected EntityId(TId value)
+        {
+            Value = value;
+        }
+
+        public override IEnumerable<object?> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+
+        public override string? ToString() => Value?.ToString() ?? base.ToString();
+
+#pragma warning disable CS8618
+        protected EntityId()
+        {
+        }
+#pragma warning restore CS8618
+    }
+}
