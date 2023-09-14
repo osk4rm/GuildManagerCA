f34ac405d6074e340a9a13959357dc936572c210
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-09-14 11:56:20 +0200

    uri extension for propertybuilder

---
+++ GuildManagerCA.Infrastructure/Persistence/EFUtils/PropertyBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuildManagerCA.Infrastructure.Persistence.EFUtils
+{
+    public static class PropertyBuilderExtensions
+    {
+        public static PropertyBuilder<Uri> HasUriConversion(this PropertyBuilder<Uri> builder)
+        {
+            return builder.HasConversion(
+                uri => uri.ToString(),
+                str => new Uri(str));
+        }
+    }
+}
