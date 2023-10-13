030c180b201a488f57f0a033912628cc7caca2c9
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-10-14 01:04:59 +0200

    more tests

--- GuildManagerCA.Tests.Integration/Api/Specialization/SpecializationTests.cs
+++ GuildManagerCA.Tests.Integration/Api/Specialization/SpecializationTests.cs
@@ -7,6 +7,7 @@ using GuildManagerCA.Domain.SpecializationAggregate.Enum;
 using GuildManagerCA.Tests.Integration.Api.Specialization.TestData;
 using GuildManagerCA.Tests.Integration.Setup;
 using GuildManagerCA.Tests.Integration.Utils;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 namespace GuildManagerCA.Tests.Integration.Api.Specialization
@@ -65,6 +66,42 @@ namespace GuildManagerCA.Tests.Integration.Api.Specialization
             var content = await response.Content.ReadFromJsonAsync<SpecializationResponse>();
             content.ClassName.Should().Be("class");
             content.SpecializationRole.ToLower().Should().Be("healer");
+        }
+
+        [Fact]
+        public async Task GetById_ForInvalidGuid_ReturnsErrorInvalidSpecializationId()
+        {
+            //arrange
+            string invalidGuid = "iAmNotValidGuid";
+
+            //act
+            var response = await HttpClient.GetAsync($"api/Specializations/get/{invalidGuid}");
+
+            //assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            var content = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+            content.Type.Should().Be("https://tools.ietf.org/html/rfc7231#section-6.5.1");
+            content.Title.Should().Be("Bad Request");
+            content.Status.Should().Be(400);
+            content.Errors.Count().Should().Be(1);
+            content.Errors.ContainsKey("Specialization.InvalidSpecializationId").Should().BeTrue();
+            
+        }
+
+        [Fact]
+        public async Task GetByClassName_ForValidClassName_ReturnsCorrectResult()
+        {
+            //arrange
+            await SeedDummyData();
+
+            //act
+            var response = await HttpClient.GetAsync($"api/Specializations/getbyclassname?className=hunter");
+
+            //assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var content = await response.Content.ReadFromJsonAsync<List<SpecializationResponse>>();
+            content.Count.Should().Be(1);
+            
         }
 
         private async Task SeedDummyData()
