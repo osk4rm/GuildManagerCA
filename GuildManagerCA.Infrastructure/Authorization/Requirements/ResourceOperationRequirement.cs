d2c14d3505a00aeba9b69f176e07add43137f119
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2023-11-07 22:28:31 +0100

    authorization requirement for resource/operation

---
+++ GuildManagerCA.Infrastructure/Authorization/Requirements/ResourceOperationRequirement.cs
@@ -0,0 +1,21 @@
+﻿using GuildManagerCA.Domain.Authorization.Requirements;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuildManagerCA.Infrastructure.Authorization.Requirements
+{
+    public class ResourceOperationRequirement : IAuthorizationRequirement
+    {
+        public ResourceOperationType ResourceOperationType { get; set; }
+
+        public ResourceOperationRequirement(ResourceOperationType resourceOperationType)
+        {
+            ResourceOperationType = resourceOperationType;
+        }
+
+    }
+}
---
+++ GuildManagerCA.Infrastructure/Authorization/Requirements/ResourceOperationRequirementHandler.cs
@@ -0,0 +1,42 @@
+﻿using GuildManagerCA.Application.Common.Authentication;
+using GuildManagerCA.Domain.Authorization.Requirements;
+using GuildManagerCA.Domain.CharacterAggregate;
+using GuildManagerCA.Domain.RaidEventAggregate;
+using GuildManagerCA.Domain.UserAggregate.ValueObjects;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuildManagerCA.Infrastructure.Authorization.Requirements
+{
+    public class ResourceOperationRequirementHandler : AuthorizationHandler<ResourceOperationRequirement, Character>
+    {
+        private readonly IUserContextService _userContextService;
+
+        public ResourceOperationRequirementHandler(IUserContextService userContextService)
+        {
+            _userContextService = userContextService;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ResourceOperationRequirement requirement, Character resource)
+        {
+            if (requirement.ResourceOperationType == ResourceOperationType.Create || requirement.ResourceOperationType == ResourceOperationType.Read)
+            {
+                context.Succeed(requirement);
+            }
+
+            //TODO: does this need a check?
+            var userId = UserId.Create(_userContextService.Id!.Value);
+
+            if (resource.UserId == userId)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
